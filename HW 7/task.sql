b797010aafe84b54e14cd5089f9a670e2fe0cf12
anisac03 <anisa.choudhury.3@city.ac.uk>
2024-12-07 22:40:56 +0000

    Add files via upload

--- HW 7/task.sql
+++ HW 7/task.sql
@@ -1,7 +1,119 @@
 
+-- Drop the database if it exists
+--DROP DATABASE IF EXISTS restaurant_database;
+
+-- Create the database
+--CREATE DATABASE restaurant_database;
+
+-- Use the new database
+--USE restaurant_database;
+
+-- Drop tables if they exist
+--DROP TABLE IF EXISTS restStaff, restRest_table, restBill, restRoom_management;
+
+CREATE TABLE restStaff (
+    staff_no INTEGER PRIMARY KEY,
+    first_name VARCHAR(15),
+    surname VARCHAR(15),
+    headwaiter INTEGER,
+    FOREIGN KEY (headwaiter) REFERENCES restStaff(staff_no)
+);
+
+CREATE TABLE restRest_table (
+    table_no INTEGER PRIMARY KEY,
+    no_of_seats INTEGER,
+    room_name VARCHAR(5)
+);
+
+CREATE TABLE restRoom_management (
+    room_name VARCHAR(15),
+    room_date INTEGER,
+    headwaiter INTEGER,
+    FOREIGN KEY (headwaiter) REFERENCES restStaff (staff_no),
+    PRIMARY KEY (room_name, room_date)
+);
+
+create table restBill (
+    bill_no INTEGER PRIMARY KEY,
+    bill_date INTEGER,
+    bill_total DECIMAL(6,2),
+    cust_name CHAR(20),
+    covers INTEGER,
+    table_no INTEGER,
+    waiter_no INTEGER,
+    FOREIGN KEY (table_no) REFERENCES restRest_table (table_no),
+    FOREIGN KEY (waiter_no) REFERENCES restStaff (staff_no)
+);
+
+
+INSERT INTO restStaff VALUES
+    (005, 'Alphonso', 'Moss', NULL),
+    (006, 'Jack', 'Hunt', NULL),
+    (010, 'Charles','Watson', NULL),
+    (001, 'John', 'Paul', 005),
+    (002, 'Paul', 'Smith', 006),
+    (003, 'Zoe', 'Ball', 005),
+    (004, 'Sam', 'Pitt', 006),
+    (007, 'Jimmy', 'Smith', 005),
+    (008, 'Tim', 'Jackson', 005),
+    (009, 'David', 'Campbell', 006),
+    (011, 'Chris', 'Smart', 010);
+
+INSERT INTO restRest_table VALUES
+    (001, 7, 'Blue'),
+    (002, 6, 'Blue'),
+    (003, 10, 'Blue'),
+    (004, 7, 'Red'),
+    (005, 4, 'Red'),
+    (006, 7, 'Red'),
+    (007, 6, 'Red'),
+    (008, 6, 'Green'),
+    (009, 5, 'Green');
+
+INSERT INTO restRoom_management VALUES
+    ('Blue', 160312, 005),
+    ('Blue', 160105, 005),   
+    ('Blue', 160210, 006),
+    ('Blue', 160215, 005),
+    ('Blue', 150614, 005),
+    ('Red', 160307, 006),
+    ('Red', 151231, 006),
+    ('Red', 160111, 006),
+    ('Red', 160312, 005),
+    ('Red', 160210, 005),
+    ('Red', 160115, 006),
+    ('Green', 160105, 010),
+    ('Green', 160111, 010),
+    ('Green', 160215, 010),
+    ('Green', 151231, 010);
+       
+INSERT INTO restBill VALUES
+    (00001, 160312, 200.99, 'Tony Beebee', 3, 001, 002),
+    (00002, 160105, 500.47, 'David Hookman', 7, 002, 003),         
+    (00003, 151231, 400.33, 'Jack Pitt', 4, 005, 004),         
+    (00004, 151231, 600.91, 'Bob Crow', 2, 004, 004),             
+    (00005, 150614, 400.23, 'Beck Smith', 7, 006, 002),
+    (00007, 160111, 237.37, 'Terry Jones', 4, 004, 002),
+    (00008, 160111, 396.00, 'Tony Beebee', 2, 005, 004),
+    (00009, 160111, 101.00, 'Tanya Singh', 1, 006, 004),
+    (00010, 160111, 272.01, 'Bob Crow', 3, 008, 011),
+    (00011, 160111, 777.11, 'Nerida Smith', 5, 009, 011),
+    (00012, 160312, 1665.27, 'David Hookman', 10, 003, 003),
+    (00013, 160312, 831.00, 'Tanya Singh', 4, 001, 008),
+    (00014, 151231, 555.66, 'Terry Jones', 3, 008, 011),
+    (00015, 151231, 102.35, 'Sunil Shah', 2, 009, 011),
+    (00016, 160111, 232.11, 'Bob Crow', 4, 009, 011),
+    (00017, 160210, 311.11, 'Tanya Singh', 2, 001, 003),
+    (00018, 160210, 89.99, 'Bob Crow', 3, 006, 009),
+    (00019, 160210, 109.31, 'Nerida Smith', 2, 008, 011),
+    (00020, 160215, 444.44, 'Bob Crow', 4, 001, 007),
+    (00021, 160215, 131.11, 'Nancy Smith', 2, 009, 011),
+    (00022, 160312, 545.01, 'Sunil Shah', 4, 006, 004);
+
+
 -- Task 7:
 
--- 1) Creating the view `samsBills` to display the bills taken by Sam Pitt
+-- 1) 
 CREATE VIEW samsBills AS
 SELECT restStaff.first_name, restStaff.surname, restBill.bill_date, restBill.cust_name, restBill.bill_total  
 FROM restBill                  
@@ -9,11 +121,11 @@ JOIN restStaff ON restBill.waiter_no = restStaff.staff_no  -- joining 'restBill'
 WHERE restStaff.first_name = 'Sam' AND restStaff.surname = 'Pitt'; -- filtering the data to include only bills taken by Sam Pitt
 SELECT * FROM samsBills; -- need to select all from 'samBills' so that the bills Sam Pitt has taken can be displayed
 
--- 2) Selecting all bills from the 'samsBills' view where the bill total is greater than £400
+-- 2) 
 SELECT * FROM samsBills
 WHERE bill_total > 400;  -- filtering bills with a total greater than £400
 
--- 3) Creating the view 'roomTotals' to show the total sum of bills for each room
+-- 3) 
 CREATE VIEW roomTotals AS
 SELECT restRest_table.room_name,  
 SUM(restBill.bill_total) AS total_sum  -- total sum of bills for that room
@@ -22,7 +134,7 @@ JOIN restRest_table ON restBill.table_no = restRest_table.table_no  -- joining '
 GROUP BY restRest_table.room_name;  -- grouping the results by room name to get total sums for each room
 SELECT * FROM roomTotals; -- need to select all rows from the 'roomTotals' view to display the room totals
 
--- 4) Creating the view 'teamTotals' to show the total sum of bills for each team
+-- 4) 
 CREATE VIEW teamTotals AS
 SELECT CONCAT(restStaff.first_name, ' ', restStaff.surname) AS headwaiter_name, -- concatenates first and last name of the waiter to create the full name
 SUM(restBill.bill_total) AS total_sum  -- total sum of bills for that team
